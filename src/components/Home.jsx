a4ed80bf50c79d53c4a6e1ca2049055b2edaac56
FahriEfendi <fahriefendi0795@gmail.com>
2024-03-22 11:36:49 +0700

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
 
     return (
         <main>
-            <Disclosure as="nav" className="bg-gray-800">
+            <Disclosure as="nav" className="bg-gray-800 bg">
                 {({ open }) => (
                     <>
                         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
