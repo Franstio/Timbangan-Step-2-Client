699fb43cc1b72d0ba6913af7467a85aecbdadba8
FahriEfendi <fahriefendi0795@gmail.com>
2024-03-22 11:32:37 +0700

    fix code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 
-import React, {Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { FaCircle } from 'react-icons/fa';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { IoSettingsOutline } from "react-icons/io5";
+
+
 
 const Home = () => {
     const navigation = [
         { name: 'Dashboard', href: '#', current: true },
-        { name: 'Team', href: '#', current: false },
-        { name: 'Projects', href: '#', current: false },
-        { name: 'Calendar', href: '#', current: false },
+        { name: 'Calculation', href: '#', current: false }
     ]
 
     function classNames(...classes) {
@@ -78,11 +79,7 @@ const Home = () => {
                                             <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                                 <span className="absolute -inset-1.5" />
                                                 <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="h-8 w-8 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
+                                                <IoSettingsOutline fontSize="1.5em" style={{ color: 'white' }} />
                                             </Menu.Button>
                                         </div>
                                         <Transition
@@ -155,68 +152,71 @@ const Home = () => {
             </Disclosure>
             <div className='bg-gray-400 p-5'>
                 <div class="flex justify-center gap-10">
-                    <div className='flex-1 p-4 border rounded flex justify-stretch bg-white'>
-                        <div className='flex-1 p-4 border rounded bg-gray-400 text-center text-5xl font-semibold max-w-xl'>10.00
-                        </div>
+                    <div className='flex-1 p-4 border rounded bg-white'>
+                    <h1 className='text-center text-blue-600 font-semibold'>Weight</h1>
+                    <div class='flex justify-warp'>
+                        <div class='flex-1 p-4 border rounded bg-gray-300 text-center text-5xl font-semibold max-w-xl'>10.00</div>
                         <p className='flex items-center text-2xl font-bold'>Kg</p>
                     </div>
+                    </div>
+
                     <div className='flex-1 p-4 border rounded max-w-md bg-white'>
-                        <h1 className='text-center font-semibold'>Status</h1>
+                        <h1 className='text-center text-blue-600 font-semibold'>Status</h1>
 
                         <div className='flex justify-between'>
                             <p className=''>Green</p>
-                            <FiberManualRecordIcon  fontSize="small" style={{ color: 'green' }}/>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green' }} />
                         </div>
                         <div className='flex justify-between'>
                             <p>Yellow</p>
-                            <FiberManualRecordIcon  fontSize="small" style={{ color: 'red' }}/>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'red' }} />
                         </div>
                         <div className='flex justify-between'>
                             <p>Red</p>
-                            <FiberManualRecordIcon  fontSize="small" style={{ color: 'red' }}/>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'red' }} />
                         </div>
                     </div>
                 </div>
 
                 <div class="flex justify-center gap-10 mt-10">
                     <div className='flex-1 p-4 border rounded bg-white'>
-                        <h1 className='text-center font-semibold'>Sensor Status</h1>
+                        <h1 className='font-semibold text-blue-600 text-center'>Sensor Status</h1>
                         <div className='flex justify-between'>
-                            <p className=''>Atas</p>
-                            <FaCircle className="icon" />
+                            <p className=''>Top</p>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'red' }} />
                         </div>
 
                         <div className='flex justify-between'>
-                            <p>Bawah</p>
-                            <FaCircle className="icon" />
+                            <p>Bottom</p>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green' }} />
                         </div>
                     </div>
                     <div className='flex-1 p-4 border rounded max-w-md bg-white'>
-                        <h1 className='text-center font-semibold'>Lock Status</h1>
+                        <h1 className='text-center font-semibold text-blue-600'>Lock Status</h1>
 
                         <div className='flex justify-between'>
-                            <p className=''>Atas</p>
-                            <FaCircle className="icon" />
+                            <p className=''>Top</p>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green' }} />
                         </div>
 
                         <div className='flex justify-between'>
-                            <p>Bawah</p>
-                            <FaCircle className="icon" />
+                            <p>Bottom</p>
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'red' }} />
                         </div>
                     </div>
 
-                    <button className='flex-1 p-4 border rounded max-w-xs flex justify-center items-center bg-white'>Lock Bottom
+                    <button className='flex-1 p-4 border rounded max-w-xs flex justify-center items-center bg-white font-semibold'>Lock Bottom
                     </button>
                 </div>
-               {/*  <footer className='flex-1 rounded border mt-10 flex justify-center gap-40 p-3 bg-white'  >
+                {/*  <footer className='flex-1 rounded border mt-10 flex justify-center gap-40 p-3 bg-white'  >
                     <p>Server Status: 192.168.1.5 Online</p>
                     <p>Status PLC : Online</p>
                 </footer> */}
             </div>
             <footer className='flex-1 rounded border flex justify-center gap-40 p-3 bg-white'  >
-                    <p>Server Status: 192.168.1.5 Online</p>
-                    <p>Status PLC : Online</p>
-                </footer>
+                <p>Server Status: 192.168.1.5 Online</p>
+                <p>Status PLC : Online</p>
+            </footer>
         </main>
     );
 };
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,13 +4,12 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { FaCircle } from 'react-icons/fa';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { IoSettingsOutline } from "react-icons/io5";
 
 const Setting = () => {
     const navigation = [
         { name: 'Dashboard', href: '#', current: true },
-        { name: 'Team', href: '#', current: false },
-        { name: 'Projects', href: '#', current: false },
-        { name: 'Calendar', href: '#', current: false },
+        { name: 'Calculation', href: '#', current: false }
     ]
 
     function classNames(...classes) {
@@ -78,11 +77,7 @@ const Setting = () => {
                                             <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                                 <span className="absolute -inset-1.5" />
                                                 <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="h-8 w-8 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
+                                                <IoSettingsOutline fontSize="1.5em" style={{ color: 'white' }} />
                                             </Menu.Button>
                                         </div>
                                         <Transition
@@ -159,22 +154,28 @@ const Setting = () => {
 
                     <h2 className='text-lg '>Tower Lamp</h2>
                     <div className='flex flex-warp gap-3'>
-                        <button className='border rounded bg-gray-400 w-20 py-2'>Red
-                        </button>
-                        <button className='border rounded bg-gray-400 w-20 py-2'>Yellow
-                        </button>
-                        <button className='border rounded bg-gray-400 w-20 py-2'>Green
-                        </button>
-                    </div>
-
-                    <h2 className='text-lg'>Lock Button</h2>
-                    <div className='flex flex-warp gap-3 relative'>
-                        <button className='border rounded bg-gray-400 w-20 py-2 relative'>
-                            Atas
+                    <button className='border rounded bg-gray-400 w-20 py-2 relative'>
+                            Green
                             <FiberManualRecordIcon fontSize="small" style={{ color: 'green', position: 'absolute', top: 0, right: 0 }} />
                         </button>
                         <button className='border rounded bg-gray-400 w-20 py-2 relative'>
-                            Bawah
+                            Yellow
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green', position: 'absolute', top: 0, right: 0 }} />
+                        </button>
+                        <button className='border rounded bg-gray-400 w-20 py-2 relative'>
+                            Red
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green', position: 'absolute', top: 0, right: 0 }} />
+                        </button>
+                    </div>
+
+                    <h2 className='text-lg mt-5'>Lock Button</h2>
+                    <div className='flex flex-warp gap-3 relative'>
+                        <button className='border rounded bg-gray-400 w-20 py-2 relative'>
+                            Top
+                            <FiberManualRecordIcon fontSize="small" style={{ color: 'green', position: 'absolute', top: 0, right: 0 }} />
+                        </button>
+                        <button className='border rounded bg-gray-400 w-20 py-2 relative'>
+                            Bottom
                             <FiberManualRecordIcon fontSize="small" style={{ color: 'red', position: 'absolute', top: 0, right: 0 }} />
                         </button>
                     </div>
