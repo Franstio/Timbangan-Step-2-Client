f29d14025f0fb47e3eb8b0570b366a69d801b453
Franstio <franstiodiego01@gmail.com>
2024-06-23 15:24:11 +0700

    Update

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -55,7 +55,7 @@ const Home = () => {
     const [bottomLockHostData, setBottomLockData] = useState({ binId: '', hostname: '' });
     const [socket, setSocket] = useState(); // Sesuaikan dengan alamat server
     const [rackTarget, setRackTarget] = useState('localhost:5001');
-    const [apiTarget, setApiTarget] = useState('localhost');
+    const [apiTarget, setApiTarget] = useState('192.168.22.128');
     //const ScaleName = getScaleName();
 
 
