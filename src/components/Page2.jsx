c959e5669c4e95c64807e4f4f8eecb9f3d48a383
Franstio <franstiodiego01@gmail.com>
2024-07-02 19:36:23 +0700

    Update

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, Fragment, useRef } from "react";
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -805,7 +804,7 @@ const Home = () => {
                 weight: _container.weight,
             }
         }).then(res => {
-            updateContainersstatus();
+            updateContainerstatus();
             sendDataPanasonicServer(_container.station, _container.name, '', _container.weight, 'Collection');
             setWaste(null);
             setScanData('');
