4bf28f762be69e23f6dc1ab0dd42c1dd6e38e8fb
Franstio <franstiodiego01@gmail.com>
2024-07-12 09:52:31 +0700

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -288,7 +288,7 @@ const Home = () => {
                                             showTick: true
                                         },
                                         {
-                                            limit: 81,
+                                            limit: 90,
                                             color: 'RED',
                                             showTick: true
                                         },
