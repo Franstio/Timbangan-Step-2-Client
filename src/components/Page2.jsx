57f7863c84df6fbc45457ad7980e71e14ef8ed6a
Franstio <franstiodiego01@gmail.com>
2024-08-22 17:50:11 +0700

    ok

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1254,7 +1254,7 @@ const Home = () => {
                                     <form>
                                         <p>{errDisposeMessage}</p>
                                         <div className="flex justify-center mt-5">
-                                            <button type="button" onClick ={()=>{setShowErrorDispose(false);setErrDisposeMessage(''); toggleContinueModal(true);}} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mr-2 rounded">Ok</button>
+                                            <button type="button" onClick ={()=>{setShowErrorDispose(false); toggleContinueModal(true);}} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mr-2 rounded">Ok</button>
                                         </div>
                                     </form>
                                 </div>
