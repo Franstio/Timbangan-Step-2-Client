73ef574f273e19c81d70eae3fd881cbbd1dba5b1
Franstio <franstiodiego01@gmail.com>
2024-11-20 08:50:29 +0700

    ok

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1324,9 +1324,9 @@ const Home = () => {
       } catch (err) {
         console.log(err);
 
-        const items = containers;
-        items.splice(-1, 1);
-        setContainers((prev) => [...items]);
+//        const items = containers;
+//        items.splice(-1, 1);
+  //      setContainers((prev) => [...items]);
         setFinalStep(false);
         setIsSubmitAllowed(true);
         setIdbin(-1);
