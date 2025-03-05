d106bc34cec58c93fd3fe4a002ca596890621cf1
admindpe-dharma <admindpe@dharmapacific.com>
2025-03-05 14:06:56 +0700

    Fix: Fix Restart Button

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1429,7 +1429,8 @@ const Home = () => {
       if (verif.data.isValid==1)
       {
         localStorage.clear();
-          reloadBin(true);
+        await apiClient.get(`http://${binDispose.name_hostname}.local:5000/clear-bin`);
+        window.location.reload();
       }
       else
       {     
